37d4b1ec5a757e0778aaab68d5bbc2d02480422e
Dieimes Nunes <dieimes.souza@docente.pr.senac.br>
2022-11-24 21:20:00 -0300

    Atualização da funcionaliade consultar

--- consultar.php
+++ consultar.php
@@ -1,13 +1,12 @@
 <?php
     include("conexao.php");
 
-    $sql_consultar = "SELECT * FROM tabela_receber";
+    $sql_consultar = "SELECT * FROM produto";
 
     $retorno_consulta = $mysqli->query($sql_consultar) or die($msqli->error);
 
-    $quantidade_mensagens = $retorno_consulta->num_rows;
-
-    var_dump($quantidade_mensagens);
+    $quantidade_produtos = $retorno_consulta->num_rows;
+    
 
 ?>
 <!DOCTYPE html>
@@ -21,32 +20,35 @@
         </head>
         <body>
             <div class="container">
-
                 <table class="table table-hover">
                     <thead>
                         <th>Id</th>
-                        <th>Nome</th>
-                        <th>Caminho</th>
-                        <th>Data</th>
+                        <th>Nome do produto</th>
+                        <th>Link da foto</th>
+                        <th>Descrição</th>
+                        <th>Valor</th>
                         <th>Ações</th>
                     </thead>
                 <tbody>
                     <?php
-                        if ($quantidade_mensagens == 0){?>
+                        if ($quantidade_produtos == 0){?>
                         <tr>
-                            <td colspan="5">Nenhum arquivo recebido.</td>
+                            <td colspan="">Nenhum produto cadastrado.</td>
                         </tr>
                         <?php
                             }else{
-                                while($mostrar_arquivo = $retorno_consulta ->fetch_assoc()){
-                                    $data = date("d/m/Y H:i", strtotime($mostrar_arquivo['data']));
+                                while($mostrar_arquivo = $retorno_consulta ->fetch_assoc()){                                    
                         ?>
                             <tr>
-                                <td><?php echo $mostrar_arquivo['id_receber']; ?></td>
+                                <td><?php echo $mostrar_arquivo['id_produto']; ?></td>
                                 <td><?php echo $mostrar_arquivo['nome']; ?></td>
-                                <td><?php echo $mostrar_arquivo['arquivo_caminho']; ?></td>
-                                <td><?php echo $data; ?></td>
-                                <td></td>
+                                <td><a href="<?php echo $mostrar_arquivo['foto']; ?>"><?php echo $mostrar_arquivo['foto'];?></a></td>
+                                <td><?php echo $mostrar_arquivo['descricao']; ?></td>
+                                <td><?php echo $mostrar_arquivo['valor']; ?></td>
+                                <td>
+                                    <a class="btn btn-default" href="editar_produto.php?id=<?php echo $mostrar_arquivo['id_produto']; ?>" role="button"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</a>
+                                    <a class="btn btn-default" href="deletar_produto.php?id=<?php echo $mostrar_arquivo['id_produto']; ?>" role="button"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Deletar</a>
+                                </td>
                             </tr>
                         <?php
                                 }
@@ -58,5 +60,4 @@
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     </body>
-</html>
 </html>
